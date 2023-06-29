a016c01944cc52888225dbf2822a210cd91468c1
tungluu <tungluu2203@kaist.ac.kr>
2023-06-29 21:11:04 +0900

    Change the link name in config.

--- src/sawyer_control/configs/ros_config.py
+++ src/sawyer_control/configs/ros_config.py
@@ -13,7 +13,7 @@ JOINT_NAMES = ['right_j0',
                'right_j5',
                'right_j6'
                ]
-LINK_NAMES = ['right_l2', 'right_l3', 'right_l4', 'right_l5', 'right_l6', '_hand']
+LINK_NAMES = ['right_l2', 'right_l3', 'right_l4', 'right_l5', 'right_l6', 'right_hand']
 # RESET_ANGLES = np.array(
 #     [0.15262207,  0.91435349, -2.02594233,  1.6647979, -2.41721773, 1.14999604, -2.47703505]
 # )
