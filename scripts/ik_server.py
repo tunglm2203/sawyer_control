e921c5574d2eae15c0896d7c4ac0515721100344
tungluu <tungluu2203@kaist.ac.kr>
2023-07-02 17:38:58 +0900

    Modify the ik_server to perform ee orientation rather use default value.

--- scripts/ik_server.py
+++ scripts/ik_server.py
@@ -13,9 +13,9 @@ def handle_compute_joint_angle(request):
     # Get name of joints
     joint_names = arm.joint_names()
 
-    ee_pos = request.ee_pos
-    ee_orientation = ros_config.POSITION_CONTROL_EE_ORIENTATION
-    pose = get_pose_stamped(ee_pos[0], ee_pos[1], ee_pos[2], ee_orientation)
+    ee_pos_x, ee_pos_y, ee_pos_z = request.ee_pos[0], request.ee_pos[1], request.ee_pos[2]
+    ee_ori_x, ee_ori_y, ee_ori_z, ee_ori_w = request.ee_pos[3], request.ee_pos[4], request.ee_pos[5], request.ee_pos[6]
+    pose = get_pose_stamped(ee_pos_x, ee_pos_y, ee_pos_z, ee_ori_x, ee_ori_y, ee_ori_z, ee_ori_w)
 
     reset_angles = ros_config.RESET_ANGLES
     reset_angles = dict(zip(joint_names, reset_angles))
